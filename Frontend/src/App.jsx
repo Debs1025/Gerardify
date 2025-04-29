8d462bb728bdc608506534b12ae39c67a4439d8e
Erick De Belen <erickdebelen22@gmail.com>
2025-04-29 22:21:55 +0800

    Adding pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './design/App.css'
+import './styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
